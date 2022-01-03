f6627623fb6d2897652876c8c42d698808d0ab2f
feelB12git config --global user.mname feelB12 <philippebidouze@gmail.com>
2022-01-03 14:25:35 +0100

    entité club et category

---
+++ src/Entity/Club.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ClubRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ClubRepository::class)
+ */
+class Club
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
